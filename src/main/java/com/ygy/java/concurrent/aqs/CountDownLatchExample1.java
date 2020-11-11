6cf0394da06564bb6dade4e50f46c0b261fafa76
杨光义 <yangguangyi@gridsum.com>
2020-11-11 17:08:11 +0800

    提交代码

--- src/main/java/com/ygy/java/concurrent/aqs/CountDownLatchExample1.java
+++ src/main/java/com/ygy/java/concurrent/aqs/CountDownLatchExample1.java
@@ -5,8 +5,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * CountDownLatch自身维护了一个计数器，每调用一次countDown()方法，计数器的值减一(原子操作)
- * 当计数器的值减到0时，才会运行await()方法后面的代码
+ * CountDownLatch的概念
+ *  CountDownLatch是一个同步工具类，用来协调多个线程之间的同步，或者说起到线程之间通信（而不是用作互斥的作用）
+ *  CountDownLatch能够使一个线程在等待另外一些线程完成各自工作之后，再继续执行。使用一个计数器实现。计数器初始值为线程的数量。当每一个线
+ *  程完成自己的任务后，计数器的值会减一。当计数器的值为0时，表示所有的线程都已经完成了一些任务，然后在CountDownLatch上等待的线程就可以
+ *  恢复执行接下来的任务
+ *
+ * CountDownLatch的用法
+ *  1、某一线程在开始运行前等待n个线程执行完毕。将CountDownLatch的计数器初始化为new CountDownLatch(n),每当一个任务线程执行完毕，就将
+ *  计数器减一，countdownLatch.countDown(),当计数器的值变为0时，在CountDownLatch上await()的线程就会被唤醒。一个典型应用场景就是
+ *  启动一个服务时，主线程需要等待多个组件加载完毕，之后再继续进行。
+ *  2、实现多个线程开始执行任务的最大并行性。注意是并行性，不是并发，强调的是多个线程在某一时刻同时开始执行。类似于赛跑，将多个线程放在起点，
+ *  等待发令枪响，然后同时开跑。做法是初始化一个共享的CountDownLatch(1)，将其计数器初始化为1，多个线程在开始执行任务前首先
+ *  countdownlatch.await(),当主线程调用countDown()时，计数器变为0，多个线程同时被唤醒。
+ *
+ * CountDownLatch的不足
+ *  CountDownLatch时一次性的，计数器的值只能在构造方法中初始化一次，之后没有任何机制再对其设置值，当CountDownLatch使用完毕后，
+ *  它不能再被使用
  */
 public class CountDownLatchExample1 {
 
--- src/main/java/com/ygy/java/concurrent/aqs/CyclicBarrierExample.java
+++ src/main/java/com/ygy/java/concurrent/aqs/CyclicBarrierExample.java
@@ -5,9 +5,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 项目名称：java
- * 类名称：CyclicBarrierExample
- * 类描述：
+ * CyclicBarrier的字面意思是可循环使用的屏障，类似于CountDownLatch也是个计数器，不同的是CyclicBarrier要做的事情是，让一组线程
+ * 到达一个屏障(也可以叫同步点)时被阻塞，直到最后一个线程到达屏障时，屏障才会开门，所有被屏障拦截的线程才会继续干活。之所有用循环修饰，
+ * 是因为在所有的线程释放彼此之后，这个屏障是可以重新使用的(reset()方法重置屏障点)
+ *
+ * CyclicBarrier是一种同步机制，允许一组线程相互等待，等到所有线程都到达一个屏障点才退出await()方法，它没有直接实现AQS，而是借助
+ * ReentrantLock来实现的同步机制。它是可循环使用的，而CountDownLatch是一次性的，另外它体现的语义也跟CountDownLatch不同，
+ * CountDownLatch减少计数到达条件采用的是release方式，而CyclicBarrier走向屏障点(await)采用的是Acquire方式，Acquire是会阻塞的
+ * 这也实现了CyclicBarrier的另外一个特点，只要有一个线程中断，那么屏障点就被打破，所有线程都将被唤醒(CyclicBarrier自己负责这部分实现，
+ * 不是由AQS调度的)，这样也避免了因为一个线程中断引起永远不能到达屏障点而导致其他线程一直等待。屏障点被打破的CyclicBarrier将不可再使用
+ * (会抛出BrokenBarrierException)，除非执行reset操作
+ *
+ * CountDownLatch：一个或多个线程，等待其他多个线程完成某件事之后才能执行
+ * CyclicBarrier：多个线程相互等待，直到到达同一个同步点，再继续一起执行
  */
 public class CyclicBarrierExample {
 
